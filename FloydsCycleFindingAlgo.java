b9fd447021207bc67186a6cff2e0aa681bfc5560
Arbaz alam <arbazalam6161@gmail.com>
2026-02-14 11:19:19 +0530

    add new files

---
+++ FloydsCycleFindingAlgo.java
@@ -0,0 +1,41 @@
+public class FloydsCycleFindingAlgo {
+
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public static boolean isCycle() {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String args[]) {
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+
+        // Creating cycle
+        head.next.next.next = head;
+
+        System.out.println(isCycle());
+    }
+}
