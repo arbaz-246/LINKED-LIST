9afd948de276d03230e27de042f71cb43851d426
Arbaz alam <arbazalam6161@gmail.com>
2026-02-23 10:32:44 +0530

    add new files

---
+++ DoublyLinkedList.java
@@ -0,0 +1,68 @@
+public class DoublyLinkedList {
+    public class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    // add first
+    public void AddFirst(int data) {
+        // create new node
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    // print the element
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    // remove
+    public int removeFirst() {
+        if (head == null) {
+            System.out.println("Dll is empty");
+            return Integer.MIN - VALUE;
+        }
+        if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+
+    }
+
+    public static void main(String args[]) {
+        Doublell dll = new Doublell();
+        dll.AddFirst(3);
+        dll.AddFirst(2);
+
+    }
+}
